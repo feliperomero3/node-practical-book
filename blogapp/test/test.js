5b80729011f157ae74abe4837e20f0bdc0c4e02e
Felipe Romero <feliperomero3@outlook.com>
2022-11-29 21:00:18 -0600

    Re-write tests in Expect.

--- blogapp/test/test.js
+++ blogapp/test/test.js
@@ -1,10 +1,10 @@
-const { expect } = require('chai');
+const expect = require('expect.js');
 
 // https://mochajs.org/#getting-started
 describe('Array', () => {
   describe('#indexOf()', () => {
     it('should return -1 when the value is not present', () => {
-      expect([1, 2, 3].indexOf(4)).to.equal(-1);
+      expect([1, 2, 3].indexOf(4)).to.be(-1);
     });
   });
 });
@@ -18,12 +18,12 @@ describe('String', () => {
       current = 'a,b,c'.split(',');
     });
     it('should return an array', () => {
-      expect(Array.isArray(current)).to.be.true; // jshint ignore:line
+      expect(current).to.be.an(Array);
     });
     it('should return the same array', () => {
-      expect(expected.length).to.equal(current.length, `${expected.length} is not equal to ${current.length}`);
+      expect(expected).to.have.length(current.length);
       for (let i = 0; i < expected.length; i++) {
-        expect(expected[i]).equal(current[i], `${i} element is not equal`);
+        expect(expected[i]).to.be(current[i]);
       }
     });
   });

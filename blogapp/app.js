7a53bcc758b3c5142b086bf380c2ffe78a6a8eb0
Felipe Romero <feliperomero3@outlook.com>
2023-05-17 22:29:27 -0600

    Add single article route.

--- blogapp/app.js
+++ blogapp/app.js
@@ -8,8 +8,11 @@ const options = {
   cert: fs.readFileSync(path.join(__dirname, 'server.crt'))
 };
 
+const articles = JSON.parse(fs.readFileSync(path.join(__dirname, 'db', 'articles.json')));
+
 let app = express();
 app.locals.appTitle = 'blog-express';
+app.locals.articles = articles;
 
 app.set('appname', 'blogapp');
 app.set('port', process.env.PORT || 8000);
@@ -20,7 +23,11 @@ app.use(express.static('public'));
 app.use(express.static('node_modules/jquery/dist'));
 app.use(express.static('node_modules/bootstrap/dist'));
 
-app.all('*', (req, res) => {
+app.get('/articles/:slug', (req, res, next) => {
+  let article = articles.find(a => { if (a.slug === req.params.slug) return a; });
+  res.render('article', article);
+});
+app.get('/', (req, res) => {
   res.render('index', { msg: 'Welcome to Practical Node.js!' });
 });
 
